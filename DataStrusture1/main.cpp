ab1b7688e29866ba45f67f4ce84b84464e326c96
Casdos <3538832376@qq.com>
2024-04-19 11:39:36 +0800

    update
    对初步代码进行了更改

--- DataStrusture1/main.cpp
+++ DataStrusture1/main.cpp
@@ -6,8 +6,16 @@ using namespace std;
 
 class Client {
 private:
+	string ID;
+	string name;
+	string phonenum;
+	string email;
+	string idnum; // 身份证号
+	string cardnum; // 银行卡号
+	int balance;
 
 public:
+	//Client(string ID,):ID()
 	void create();
 	void check();
 	void modify();
@@ -27,5 +35,23 @@ public:
 int main()
 {
 	vector <Client> client;
+	int system_i = -1;
+	do {
+		cout << "\t\t================================================================" << endl;
+		cout << "\t\t*                      请选择进入的系统                        *" << endl;
+		cout << "\t\t*                                                              *" << endl;
+		cout << "\t\t*               管理员                   客户                  *" << endl;
+		cout << "\t\t*                                                              *" << endl;
+		cout << "\t\t*                0                        1                    *" << endl;
+		cout << "\t\t================================================================" << endl;
+		cin >> system_i;
+		if (system_i == 0) {
+
+		}
+		if (system_i == 1) {
+
+		}
+		if (system_i != 0 && system_i != 1) cout << "非法输入，请再次输入" << endl;
+	} while (system_i != 0 && system_i != 1);
 	return 0;
 }
