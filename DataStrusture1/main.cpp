8eec0ad61690fc41c7d2866d561305cc3b9f5709
Casdos <3538832376@qq.com>
2024-04-19 15:26:59 +0800

    request 1
    完成要求1

--- DataStrusture1/main.cpp
+++ DataStrusture1/main.cpp
@@ -13,9 +13,10 @@ private:
 	string idnum; // 身份证号
 	string cardnum; // 银行卡号
 	int balance;
-
 public:
-	//Client(string ID,):ID()
+	Client(string IDn = "0", string n = "none", string phone = "0", string e = "@bit.edu", \
+		string idn = "0", string cardn = "unknown", int bal = 0) :ID(IDn), \
+		name(n), phonenum(phone), email(e), idnum(idn), cardnum(cardn), balance(bal) {};
 	void create();
 	void check();
 	void modify();
@@ -46,15 +47,16 @@ void showLogin() {
 	cout << "\t\t*                      请登陆您的账户                           *" << endl;
 	cout << "\t\t*                                                              *" << endl;
 	cout << "\t\t*					    账号：                                  *" << endl;
-	cout << "\t\t*                                                              *" << endl;
+	cout << "\t\t*                   如无账号，请申请开户（按1）                   *" << endl;
 	cout << "\t\t================================================================" << endl;
 };
 
 int main()
 {
-	vector <Client> client;
+	vector <Client> user;
+	string ID, name, phonenum, email, idnum, cardnum;
+	int balance = 0;
 	int system_i = -1;
-	string ID;
 	do {
 		showIniMean();
 		cin >> system_i;
@@ -64,6 +66,22 @@ int main()
 		if (system_i == 1) {
 			showLogin();
 			cin >> ID;
+			if (ID == "1") {
+				cout << "请设置您的身份证标识号：";
+				cin >> ID;
+				cout << "请输入您的姓名：";
+				cin >> name;
+				cout << "请输入您的电话：";
+				cin >> phonenum;
+				cout << "请输入您的电子邮箱：";
+				cin >> email;
+				cout << "请输入您的身份证号：";
+				cin >> idnum;
+				user.push_back(Client(ID, name, phonenum, email, idnum, cardnum));
+				cout << "已开户成功，点击退回登陆界面" << endl;
+				getchar();
+			}
+			showLogin();
 		}
 		if (system_i != 0 && system_i != 1) cout << "非法输入，请再次输入" << endl;
 	} while (system_i != 0 && system_i != 1);
